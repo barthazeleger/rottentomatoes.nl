ece1a7074ff3e644c022478fb4651faa88440d28
NiDiDo bv <nidido@NiDiDos-MacBook-Pro.local>
2018-10-04 15:47:15 +0200

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Dag luitjes</h1>
+    <Link to="/page-2/"></Link>
+  </Layout>
+)
+
+export default IndexPage
