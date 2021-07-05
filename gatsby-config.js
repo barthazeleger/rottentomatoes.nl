dde211e3a43439ff649a78a83ba1abdb91dc37ba
Bart <bart@hazeleger.me>
2021-07-05 08:53:33 +0200

    Update gatsby-config.js
    Deleted google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,26 +16,6 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-	{
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-107363934-2",
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Enables Google Optimize using your container Id
-        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Any additional create only fields (optional)
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "example.com",
-      },
-    },
     'gatsby-plugin-offline',
   ],
 }
